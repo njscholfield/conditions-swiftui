fee97e6de2fbdabaf8345dac32c4ff855d851f86
Noah Scholfield <contact@noahscholfield.com>
2020-08-15 13:33:23 -0400

    Initial Commit

---
+++ Shared/conditions_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  conditions_swiftuiApp.swift
+//  Shared
+//
+//  Created by Noah Scholfield on 8/15/20.
+//
+
+import SwiftUI
+
+@main
+struct conditions_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
